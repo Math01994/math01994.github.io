4b07d25316dcaee08dc178c5b1c6597c828e218e
Math01994 <84423724+Math01994@users.noreply.github.com>
2021-05-19 02:58:43 +0100

    Add files via upload

---
+++ data/Hopitaux_5.js
@@ -0,0 +1,19 @@
+var json_Hopitaux_5 = {
+"type": "FeatureCollection",
+"name": "Hopitaux_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nom": "Centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.166321034636457, 12.043809113799439 ] } },
+{ "type": "Feature", "properties": { "id": "2", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.169261096482995, 12.083009938419927 ] } },
+{ "type": "Feature", "properties": { "id": "3", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.148435658403359, 12.082519928112172 ] } },
+{ "type": "Feature", "properties": { "id": "4", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.133735349170676, 12.098690268268124 ] } },
+{ "type": "Feature", "properties": { "id": "5", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.157990859404602, 12.106530433192219 ] } },
+{ "type": "Feature", "properties": { "id": "6", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.157745854250726, 12.117310659962852 ] } },
+{ "type": "Feature", "properties": { "id": "7", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.114869952322067, 12.097955252806489 ] } },
+{ "type": "Feature", "properties": { "id": "8", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.092819488473042, 12.095995211575465 ] } },
+{ "type": "Feature", "properties": { "id": "9", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.100169643089382, 12.106285428038341 ] } },
+{ "type": "Feature", "properties": { "id": "10", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.107519797705725, 12.112410556885294 ] } },
+{ "type": "Feature", "properties": { "id": "11", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.123935143015556, 12.097220237344853 ] } },
+{ "type": "Feature", "properties": { "id": "12", "nom": "centre_sante", "coord": 12.0, "wikipedia": null, "type": "hopitaux" }, "geometry": { "type": "Point", "coordinates": [ 15.11952505024575, 12.12172075273266 ] } }
+]
+}
